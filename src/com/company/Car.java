4eaaf378dde5155ebb8514b7a34ddab9b40ec109
Pavel <impetus@yandex.ru>
2020-09-08 17:55:58 +0300

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Car {
+    String model;
+    int maxspeed;
+    String color;
+    double weight;
+    void doMove() {
+        System.out.println("Move...");
+    }
+    void doBeep() {
+        System.out.println("Beep...");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Car bmw = new Car();
+        bmw.doBeep();
+        bmw.doMove();
+    }
+}
